e7d6e201005da23a447e03a2c53c7b4be56ae973
may896868 <may896868@gmail.com>
2025-09-28 11:35:20 +0530

    ronoshop commit

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-08b1c415c84f385e1" # replace with your SG ID
+ZONE_ID="Z01548632OXV3JX169848"
+DOMAIN_NAME="dhanvi.fun"
 
 for instance in $@ # mongodb redis mysql
 do
@@ -10,10 +12,29 @@ do
     # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-       
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance : $IP" 
+
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
